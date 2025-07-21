bc1938f06e1b6236d31bff17772249dad4266cfa
Santiago Marin Cardona <160188990+SMarinC@users.noreply.github.com>
2025-07-21 00:32:56 -0500

    Update maquina_insert.php
    para que muestre las fechas de reparación como nulo y no 0000-00-00

--- laVioleta/maquina/maquina_insert.php
+++ laVioleta/maquina/maquina_insert.php
@@ -23,8 +23,16 @@ if (empty($_POST['mecanicoReparacionId'])) {
     $mecanicoReparacionIdSQL = "'" . intval($_POST['mecanicoReparacionId']) . "'";
 }
 
+if (empty($_POST['fechaUltimaReparacion'])) {
+    // Si está vacío, usamos la palabra literal NULL para la consulta
+    $fechaUltimaReparacionSQL = "NULL";
+} else {
+    // Si tiene un valor, nos aseguramos que sea un número y le ponemos comillas
+    $fechaUltimaReparacionSQL = "'" . mysqli_real_escape_string($conn, $_POST['fechaUltimaReparacion']) . "'";
+}
+
 // Query SQL a la BD. Si tienen que hacer comprobaciones, hacerlas acá (Generar una query diferente para casos especiales)
-$query = "INSERT INTO `maquina`(`codigo`, `nombre`, `fabricante`, `precio`, `fechaAdquisicion`, `descripcion`, `fechaInspeccion`, `fechaUltimaReparacion`, `mecanicoInspeccionId`, `mecanicoReparacionId`) VALUES ('$codigo', '$nombre', '$fabricante', '$precio', '$fechaAdquisicion', '$descripcion', '$fechaInspeccion', '$fechaUltimaReparacion', '$mecanicoInspeccionId', $mecanicoReparacionIdSQL)";
+$query = "INSERT INTO `maquina`(`codigo`, `nombre`, `fabricante`, `precio`, `fechaAdquisicion`, `descripcion`, `fechaInspeccion`, `fechaUltimaReparacion`, `mecanicoInspeccionId`, `mecanicoReparacionId`) VALUES ('$codigo', '$nombre', '$fabricante', '$precio', '$fechaAdquisicion', '$descripcion', '$fechaInspeccion', $fechaUltimaReparacionSQL, '$mecanicoInspeccionId', $mecanicoReparacionIdSQL)";
 
 // Ejecutar consulta
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
